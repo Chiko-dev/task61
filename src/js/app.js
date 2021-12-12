748bdd0f4e86bc460d782b8d06eb82ec242456c7
Chiko-dev <chikobg12345@gmail.com>
2021-12-12 17:38:31 +0200

    js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 
 window.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".price").forEach(function(hot) {
+        console.log(hot);
         if (hot.classList.contains(".hot")){
            hot.append("🔥");
         }
