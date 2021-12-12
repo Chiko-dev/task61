80dd92408700853a199945e0761fd40dd5809ee0
Chiko-dev <chikobg12345@gmail.com>
2021-12-12 16:56:26 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorA,ll("hot").forEach(hot, () => {
+  document.querySelectorAll("hot").forEach(hot, () => {
       for ( let i = 0; i < hot.length; i++){
         hot[i].append("test");
       }
