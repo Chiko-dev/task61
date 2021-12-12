11eea018fdb63fee0b1d0bc6216e646b30f16887
Chiko-dev <chikobg12345@gmail.com>
2021-12-12 17:31:27 +0200

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 
 window.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".hot").forEach(hot, () => {
+        console.log(hot.length);
+        console.log(hot);
         for (let i = 0; i < hot.length; i++){
            hot[i].append("🔥");
         }
