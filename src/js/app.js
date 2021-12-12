bb605d6fd2ba3fbbfa30d80848a649dd39cf9969
Chiko-dev <chikobg12345@gmail.com>
2021-12-12 17:56:02 +0200

    js fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@ window.addEventListener("DOMContentLoaded", () => {
     let hotPrice = document.querySelectorAll(".price");
 
     for (let i = 0; i < hotPrice.length; i++){
-        if(hotPrice.classList.contains('hot')){
-            hotPrice.append("🔥");
+        if(hotPrice[i].classList.contains('hot')){
+            hotPrice[i].append("🔥");
         }
     }
 });
